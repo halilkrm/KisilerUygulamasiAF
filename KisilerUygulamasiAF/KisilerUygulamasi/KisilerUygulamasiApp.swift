a8d811a82d2d9cade3cbbff0f938a6a3b1079b16
Halilkrm <117026780+halilkrm@users.noreply.github.com>
2025-05-29 17:30:11 +0300

    Add files via upload

---
+++ KisilerUygulamasiAF/KisilerUygulamasi/KisilerUygulamasiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KisilerUygulamasiApp.swift
+//  KisilerUygulamasi
+//
+//  Created by Kasım on 17.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct KisilerUygulamasiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Anasayfa()
+        }
+    }
+}
